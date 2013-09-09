4fa2cab193c7553173f3faf12a69b25519c2cd33
secynic <secynic@gmail.com>
2013-09-09 13:14:13 -0500

    Fixed README issue, version Incremented 0.1.1

--- ipwhois/__init__.py
+++ ipwhois/__init__.py
@@ -21,7 +21,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 import ipaddress, socket, urllib.request, dns.resolver, re
 from xml.dom.minidom import parseString
