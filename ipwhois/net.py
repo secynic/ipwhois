6f6d5b7fc38b70d0e756563c51ba04b5747795f2
David Ford <david@blue-labs.org>
2016-01-31 00:11:21 +0000

    use our own instance of dns.resolver.Resolver() which lets us use our timeout value

--- ipwhois/net.py
+++ ipwhois/net.py
@@ -111,6 +111,10 @@ class Net:
         # Default timeout for socket connections.
         self.timeout = timeout
 
+        self.resolver = dns.resolver.Resolver()
+        self.resolver.timeout  = timeout
+        self.resolver.lifetime = timeout
+
         # Proxy opener.
         if isinstance(proxy_opener, OpenerDirector):
 
@@ -213,7 +217,7 @@ class Net:
             if result is None:
 
                 log.debug('ASN query for {0}'.format(self.dns_zone))
-                data = dns.resolver.query(self.dns_zone, 'TXT')
+                data = self.resolver.query(self.dns_zone, 'TXT')
                 temp = str(data[0]).split('|')
 
             else:
@@ -579,6 +583,7 @@ class Net:
         # Attempt to resolve ASN info via Cymru. DNS is faster, try that first.
         try:
 
+            self.resolver.lifetime = self.resolver.timeout * retry_count
             asn_data = self.get_asn_dns()
 
         except (ASNLookupError, ASNRegistryError):
