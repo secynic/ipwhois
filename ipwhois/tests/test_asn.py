e947df634b55ec99a9ce141354327749724292f3
Haseeb Majid <hmajid2301@gmail.com>
2019-02-05 11:14:46 +0000

    Changes from secynic
    Changes from secynic.

--- ipwhois/tests/test_asn.py
+++ ipwhois/tests/test_asn.py
@@ -136,30 +136,7 @@ class TestIPASN(TestCommon):
             self.fail('Unexpected exception raised: {0}'.format(e))
 
     def test_lookup(self):
-        data_dir = path.dirname(__file__)
-
-        with io.open(str(data_dir) + '/asn.json', 'r') as \
-                data_file:
-            data = json.load(data_file)
-
-        for key, val in data.items():
-
-            log.debug('Testing: {0}'.format(key))
-            net = Net(key)
-            obj = IPASN(net)
-
-            try:
-
-                self.assertIsInstance(obj.lookup(), dict)
-                self.assertIsInstance(obj.lookup(asn_alts=['http']), dict)
-
-            except AssertionError as e:
-
-                raise e
-
-            except Exception as e:
-
-                self.fail('Unexpected exception raised: {0}'.format(e))
+        raise NotImplemented
 
 
 class TestASNOrigin(TestCommon):
