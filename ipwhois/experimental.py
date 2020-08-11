a3253af24a3d67cafd2a0d90dfa01c239dee4142
Imed Mnif <imed.mnif@dreamlab.net>
2020-08-11 23:28:09 +0100

    Add asn data to rdap results

--- ipwhois/experimental.py
+++ ipwhois/experimental.py
@@ -373,7 +373,8 @@ def bulk_lookup_rdap(addresses=None, inc_raw=False, retry_count=3, depth=0,
                         # Lookup was successful, add to result. Set the nir
                         # key to None as this is not supported
                         # (yet - requires more queries)
-                        results[ip] = rdap_result
+                        results[ip]['asn'] = asn_data
+                        results[ip]['rdap'] = rdap_result
                         results[ip]['nir'] = None
 
                         # Remove the IP from the lookup queue
