09de769d2aca83fce41f177fe5a05674d6891d56
secynic <secynic@gmail.com>
2024-10-15 00:41:36 -0500

    v1.3.0

--- setup.py
+++ setup.py
@@ -59,6 +59,10 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
     'Topic :: Internet',
     'Topic :: Software Development',
 ]
